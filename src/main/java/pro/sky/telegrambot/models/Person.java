ee98966bbc7cd99c6bbcd6ce2cece5a46f31177d
Berychc <treewincsgo@gmail.com>
2024-05-27 12:07:00 +0300

    Добавление сущности в Model

--- src/main/java/pro/sky/telegrambot/models/Person.java
+++ src/main/java/pro/sky/telegrambot/models/Person.java
@@ -1,13 +1,16 @@
 package pro.sky.telegrambot.models;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.util.Objects;
 
+@Entity
 public class Person {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long personId;
 
     private Long chatId;
