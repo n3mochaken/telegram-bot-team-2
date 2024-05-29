fd44f1ab2e448c4de4d8df397db278fbccad9313
Berychc <treewincsgo@gmail.com>
2024-05-29 23:58:41 +0300

    КНОПКИ - будет дорабатываться(demo)

--- src/main/java/pro/sky/telegrambot/service/ServiceCommand.java
+++ src/main/java/pro/sky/telegrambot/service/ServiceCommand.java
@@ -6,14 +6,17 @@ import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.model.request.InlineKeyboardButton;
 import com.pengrad.telegrambot.model.request.InlineKeyboardMarkup;
+import com.pengrad.telegrambot.request.DeleteMessage;
 import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.BaseResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pro.sky.telegrambot.listener.TelegramBotUpdatesListener;
 
-import static pro.sky.telegrambot.constants.Constants.INFO;
+import static pro.sky.telegrambot.constants.Constants.*;
+
 
 
 @Service
@@ -35,14 +38,26 @@ public class ServiceCommand {
         }
     }
 
-    public void startCommand(long chatId, String message) {
-        String text = "Привет!." +
+    public void startCommand(long chatId) {
+        String text = "Привет!\n" +
                 "\nЯ бот приютитель, могу помочь тебе приютить животное." +
-                "\nУ меня есть вся подробная информация о процессе взаимодейсвия с приютами." +
-                "\nЕсли тебе нуждна помощь напиши команду \n/help";
+                "\nУ меня есть вся подробная информация о процессе взаимодейсвия с приютами.";
 
-        String formattedText = String.format(text, message);
-        sendMessage(chatId, formattedText);
+        sendMessage(chatId, text);
+
+        InlineKeyboardMarkup keyboardMarkup = new InlineKeyboardMarkup();
+        InlineKeyboardButton infoBtn = new InlineKeyboardButton("Информация о приютах").callbackData(INFO);
+        keyboardMarkup.addRow(infoBtn);
+
+        SendMessage sendMessage1 = new SendMessage(chatId, "Выберите , что вам необходимо!");
+        sendMessage1.replyMarkup(keyboardMarkup);
+
+        bot.execute(sendMessage1);
+
+        // Доработаю!!!!
+
+        // Тут много мата XD
+
     }
 
     public void infoPr(long chatId) {
@@ -52,19 +67,9 @@ public class ServiceCommand {
 
     public void helpCommand(long chatId) {
 
-        InlineKeyboardMarkup keyboardMarkup = new InlineKeyboardMarkup();
-        InlineKeyboardButton infoBtn = new InlineKeyboardButton("Информация о приютах").callbackData("/info");
-        InlineKeyboardButton volonterBtn = new InlineKeyboardButton("Волонтер").callbackData("Volonter");
-        keyboardMarkup.addRow(infoBtn);
-        keyboardMarkup.addRow(volonterBtn);
-
-        SendMessage sendMessage1 = new SendMessage(chatId, "Выбери кнопку");
-        sendMessage1.replyMarkup(keyboardMarkup);
-        bot.execute(sendMessage1);
-
     }
 
-    private Long getChatId (Message message){
+    private Long getChatId(Message message){
             return message.chat().id();
     }
 }
