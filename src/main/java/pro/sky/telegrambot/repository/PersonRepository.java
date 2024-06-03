fe443d6daa672d19686a2022f09ae362c4501fc0
n3mochaken <ns.mixa@gmail.com>
2024-06-03 14:36:53 +0300

    here we go again.

--- src/main/java/pro/sky/telegrambot/repository/PersonRepository.java
+++ src/main/java/pro/sky/telegrambot/repository/PersonRepository.java
@@ -6,5 +6,5 @@ import pro.sky.telegrambot.entity.person.Person;
 import java.util.List;
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
-    List<Person> findByTelegramId(Long telegramId);
+
 }
