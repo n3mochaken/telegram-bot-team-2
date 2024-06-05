e973acb86d0229fe0398a638892e2fa692bce2c1
n3mochaken <ns.mixa@gmail.com>
2024-06-05 13:19:16 +0300

    Переделаны константы комманд. В сервис команд переделан метод получения информации о приюте. В пропертях добавлен путь к файлу(его нужно сделать динамическим) Изменения в лисенере - закоменчены неспользующиеся команды+добавлены колбэки для меню

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -4,12 +4,10 @@ import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.CallbackQuery;
 import com.pengrad.telegrambot.model.Update;
-import com.pengrad.telegrambot.request.SendMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import pro.sky.telegrambot.entity.Person;
 import pro.sky.telegrambot.repository.PersonRepository;
 import pro.sky.telegrambot.service.ServiceCommand;
 import pro.sky.telegrambot.service.entities.PersonService;
@@ -63,17 +61,29 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
 
-            commandMap.put(START, chatId -> {
+            commandMap.put(START_COMMAND, chatId -> {
                 service.startCommand(update);
                 logger.info("Command called - /start");
             });
-            commandMap.put(INFO, chatId -> {
-                service.infoPr(update);
-                logger.info("Command called - /info");
+//            commandMap.put(INFO_COMMAND, chatId -> {
+//                service.infoPr(update);
+//                logger.info("Command called - /info");
+//            });
+//            commandMap.put(VOLUNTEER_COMMAND, chatId -> {
+//                service.volunteerCommand(update);
+//                logger.info("Command called - /volunteer");
+//            });
+            commandMap.put(CALL_BACK_GET_FILE_GENERAL, chatId -> {
+                service.sendFileToUser(update);
+                logger.info("Command called - CALL_BACK_GET_FILE_GENERAL");
             });
-            commandMap.put(VOLUNTEER, chatId -> {
+            commandMap.put(CALL_BACK_FOR_INFO, chatId -> {
+                service.infoPr(update);
+                logger.info("Command called - CALL_BACK_FOR_INFO");
+            });
+            commandMap.put(CALL_BACK_FOR_VOLUNTEER, chatId -> {
                 service.volunteerCommand(update);
-                logger.info("Command called - /volunteer");
+                logger.info("Command called - CALL_BACK_FOR_VOLUNTEER");
             });
 
 
@@ -88,7 +98,6 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
                 }
             }
             if (update.callbackQuery() != null) {
-
                 logger.info("Мы получили апдейт, колбэн не нулл");
                 CallbackQuery callbackQuery = update.callbackQuery();
                 String callbackData = callbackQuery.data();
