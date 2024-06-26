86680ca9329fe484c068c4226299d8081390f5ba
n3mochaken <ns.mixa@gmail.com>
2024-06-26 14:15:49 +0300

    апдейт репы

--- src/main/java/pro/sky/telegrambot/repository/AnimalRepository.java
+++ src/main/java/pro/sky/telegrambot/repository/AnimalRepository.java
@@ -1,6 +1,7 @@
 package pro.sky.telegrambot.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pro.sky.telegrambot.entity.Animal;
 
@@ -11,5 +12,8 @@ import java.util.List;
  */
 @Repository
 public interface AnimalRepository extends JpaRepository<Animal, Long> {
+
+    @Query(value = "SELECT * FROM animal ORDER BY RANDOM() LIMIT 1", nativeQuery = true)
+    Animal findRandomAnimal();
 
 }
